9cb9eef2869c3e7c76b13f34d50ae251821b34ec
imanelbanna <82767304+imanle@users.noreply.github.com>
2022-09-27 21:03:17 +0300

    ya rab

--- kernel-2.cu
+++ kernel-2.cu
@@ -9,23 +9,27 @@ __constant__ float filter_c[FILTER_DIM][FILTER_DIM];
 
 __global__ void convolution_tiled_kernel(float* input, float* output, unsigned int width, unsigned int height) {
 
-    // TODO
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+     __shared__ float cov[IN_TILE_DIM][IN_TILE_DIM];
+     unsigned int row = blockIdx.y*blockDim.y + threadIdx.y;
+     unsigned int col = blockIdx.x*blockDim.x + threadIdx.x;
+     float sum = 0.0f;
+     for(unsigned int tile = 0; tile < (width + TILE_DIM -1 ) / TILE_DIM; ++tile) {
+        if((row >= 0) && (row< height) && (col >= 0) && (col < width) ) {
+        cov[threadIdx.y][threadIdx.x]= cov[row*width + tile*TILE_DIM + threadIdx.x];
+    }
+    else{
+     cov[threadIdx.y][threadIdx.x]=0;
+     }
+     if(threadIdx.y < TILE_DIM && threadIdx.x < TILE_DIM){
+        for(i = 0; i < FILTER_DIM; i++) {
+            for(j = 0; j < FILTER_DIM; j++) { 
+                output += filter_c_[i][j] * cov[i+threadIdx.y][j+threadIdx.x];
+} }
+     
+    output[outRow*width + outCol] = sum;
+     }
+     
+     
 
 
 
@@ -41,7 +45,7 @@ void copyFilterToGPU(float filter[][FILTER_DIM]) {
 
     // Copy filter to constant memory
 
-    // TODO
+    cudaMemcpyToSymbol(filter_c, filter, FILTER_DIM*FILTER_DIM*sizeof(float));
 
 }
 
@@ -49,7 +53,9 @@ void convolution_tiled_gpu(float* input_d, float* output_d, unsigned int width, 
 
     // Call kernel
 
-    // TODO
+    dim3 numThreadsPerBlock(OUT_TILE_DIM, OUT_TILE_DIM);
+    dim3 numBlocks((width + OUT_TILE_DIM - 1)/OUT_TILE_DIM, (height + OUT_TILE_DIM - 1)/OUT_TILE_DIM);
+    convolution_tiled_kernel <<< numBlocks, numThreadsPerBlock >>> (input_d, output_d, width, height);
 
 
 
