148fedf980e4d83a84f70124c95bbd00c18bc9ed
imanelbanna <82767304+imanle@users.noreply.github.com>
2022-09-28 13:39:03 +0300

    ya rab

--- kernel-2.cu
+++ kernel-2.cu
@@ -10,34 +10,29 @@ __constant__ float filter_c[FILTER_DIM][FILTER_DIM];
 __global__ void convolution_tiled_kernel(float* input, float* output, unsigned int width, unsigned int height) {
 
      __shared__ float cov[IN_TILE_DIM][IN_TILE_DIM];
-     unsigned int row = blockIdx.y*blockDim.y + threadIdx.y;
-     unsigned int col = blockIdx.x*blockDim.x + threadIdx.x;
+      int out_row = threadIdx.y + blockIdx.y * OUT_TILE_DIM;
+      int out_col = threadIdx.x + blockIdx.x * OUT_TILE_DIM;
+      int in_row = out_row - FILTER_DIM;
+      int in_col = out_col - FILTER_DIM;
+     
      float sum = 0.0f;
-     for(unsigned int tile = 0; tile < (width + TILE_DIM -1 ) / TILE_DIM; ++tile) {
-        if((row >= 0) && (row< height) && (col >= 0) && (col < width) ) {
-        cov[threadIdx.y][threadIdx.x]= cov[row*width + tile*TILE_DIM + threadIdx.x];
-    }
+        if((in_row >= 0) && (in_row< height) && (in_col >= 0) && (in_col < width) ) {
+        cov[threadIdx.y][threadIdx.x]= cov[in_row*width + in_col];
+    
     else{
      cov[threadIdx.y][threadIdx.x]=0;
      }
-     if(threadIdx.y < TILE_DIM && threadIdx.x < TILE_DIM){
+           __syncthreads();
+     if(threadIdx.y < OUT_TILE_DIM && threadIdx.x < OUT_TILE_DIM){
         for(i = 0; i < FILTER_DIM; i++) {
             for(j = 0; j < FILTER_DIM; j++) { 
-                output += filter_c_[i][j] * cov[i+threadIdx.y][j+threadIdx.x];
+                sum += filter_c_[i][j] * cov[i+threadIdx.y][j+threadIdx.x];
 } }
-     
-    output[OUT_TILE_DIM*width + OUT_TILE_DIM] = sum;
+           __syncthreads();
+          if(out_row < height && out_col < width){
+    output[out_row*width + out_col] = sum;
+          }
      }
-     
-     
-
-
-
-
-
-
-
-
 
 }
 
